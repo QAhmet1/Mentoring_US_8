75bec262ccd4f9799a6593537e9547d9cbeee457
ahmet <ahmetdem6325@gmail.com>
2023-03-28 17:25:40 +0300

    dataTypes added for week2/mentoring

--- src/week2/_2_tuesday/_02_DataTypes.java
+++ src/week2/_2_tuesday/_02_DataTypes.java
@@ -13,7 +13,7 @@ public class _02_DataTypes {
         short shortValue=3000;
         int intValue=200000;
         long longValue=2222222222222333L;
-        // Default olarak rakamları int gördüğünden L ekleyerek long olduğunu söyledik.
+        // Since it sees numbers as int by default, we added L and said it was long.
 
         float floatValue=3.14F;
         double doubleValue=3.14;
@@ -26,6 +26,33 @@ public class _02_DataTypes {
         System.out.println(byteValue);  // prints 4
 
         System.out.println("byteValue=" + byteValue); // prints byteValue=4
+
+
+
+        byte byteMin= Byte.MIN_VALUE;
+        byte byteMax= Byte.MAX_VALUE;
+
+        short shortMin= Short.MIN_VALUE;
+        short shortMax= Short.MAX_VALUE;
+
+        System.out.println("byteMin = " + byteMin);
+        System.out.println("byteMax = " + byteMax);
+        System.out.println("shortMin = " + shortMin);
+        System.out.println("shortMax = " + shortMax);
+
+        System.out.println("Integer.MIN_VALUE = " + Integer.MIN_VALUE);
+        System.out.println("Integer.MAX_VALUE = " + Integer.MAX_VALUE);
+
+        System.out.println("Long.MIN_VALUE = " + Long.MIN_VALUE);
+        System.out.println("Long.MAX_VALUE = " + Long.MAX_VALUE);
+
+        System.out.println("Float.MIN_VALUE = " + Float.MIN_VALUE);
+        System.out.println("Float.MAX_VALUE = " + Float.MAX_VALUE);
+
+        System.out.println("Double.MIN_VALUE = " + Double.MIN_VALUE);
+        System.out.println("Double.MAX_VALUE = " + Double.MAX_VALUE);
+
+
 
     }
 }
