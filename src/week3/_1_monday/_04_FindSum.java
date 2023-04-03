06f0f61b88fab084ee185acee4db9e9f6c16a85f
ahmet <ahmetdem6325@gmail.com>
2023-04-03 08:54:06 +0300

    week3/mentoring

---
+++ src/week3/_1_monday/_04_FindSum.java
@@ -0,0 +1,25 @@
+package week3._1_monday;
+
+import java.util.Scanner;
+
+public class _04_FindSum {
+    public static void main(String[] args) {
+        // Find the sum of the numbers up to an entered number and print it.
+        // including the entered number
+        //for instance if the user entered 5 then the result will be 5+4+3+2+1=15
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int number=scanner.nextInt(); //5
+        int sum=0;
+        while(number>0)
+        {
+            sum=sum+number; // 5 9 12 14 15
+            number--; // 5 4 3 2 1
+
+
+        }
+
+        System.out.println("Total = " + sum);
+    }
+}
