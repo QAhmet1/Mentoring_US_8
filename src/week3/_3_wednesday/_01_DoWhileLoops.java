c76e954a4e11f9d2c5b6933424379ba162224a09
emre087 <fee0984@gmail.com>
2023-04-08 21:46:10 +0200

    For Loop

--- src/week3/_3_wednesday/_01_DoWhileLoops.java
+++ src/week3/_3_wednesday/_01_DoWhileLoops.java
@@ -1,5 +1,98 @@
 package week3._3_wednesday;
 
 public class _01_DoWhileLoops {
-    //lşdsldsş
+    public static void main(String[] args) {
+
+
+        // This is the algorithm of finding the sum with while loop from 1 to 50
+        int sum =0;
+        int i=0;
+
+        while (i<=50) {
+            sum+= i;
+            i++;
+        }
+
+        System.out.println(sum);
+
+
+        // Difference between While Looop and Do While Loop
+        // In Do While Loop if even the condition is false it is executed once.
+
+        int m =12;
+        while (m>13) {  // This is a false condition
+            System.out.println("While Loop");
+        }
+
+        System.out.println("--------------------------");
+        do {
+            System.out.println("Hello Java");   // In Do While evertime There is one execution
+        } while (m>13);
+
+        // We can use do while loop like while loop
+        // Print Hello World  Five Times
+        int j=0;
+
+        do {
+            System.out.println("Hello World");
+            j++;
+        } while (j<5);
+
+
+
+        // Comparison with String
+
+        String str = "John";
+        System.out.println("----------------");
+
+        do {
+            // implementation body
+            System.out.println("Hello John");
+
+        } while ( str.equals("John") );          //  equals is used to compare strings only
+
+
+
+        // EXAMPLE 2
+        int s =12;
+        do {
+            System.out.println("Hello World");
+
+        } while (s<5);  // Normally The condition here is false
+
+
+        // print "hello techno study" five times wiht do  while loop
+        // YOU CAN USE DO WHILE LOOP LIKE WHILE LOOP THIS WAY
+
+        int n =0;
+
+        do {
+            System.out.println("hello techno Study");
+            n++;
+        } while (n<5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
 }
--- src/week3/_4_thursday/_01_MixLoopsExamples.java
+++ src/week3/_4_thursday/_01_MixLoopsExamples.java
@@ -1,5 +1,123 @@
 package week3._4_thursday;
 
+import java.util.Scanner;
+
+// FOR LOOP PRACTICES
+
 public class _01_MixLoopsExamples {
+
+    public static void main(String[] args) {
+
+        // even numbers from 30 to 1
+        for (int i = 30; i >= 1 ; i--) {
+            if (i%2==0) {
+                System.out.println(i);
+            }
+        }
+
+
+        // Find the average of numbers from 30 to 1
+        // What is average?  2 and 4 --> sum/count --> 3
+
+        double sum = 0;
+        for (int i = 30; i >=1 ; i--) {
+            sum+=i;
+        }
+
+        double average = sum/30;
+        System.out.println("average = " + average);
+
+        System.out.println("---------------------------------");
+
+
+        // Print the letters from a to z
+        // char is the shourtcut of Character
+
+        for (char i = 'a'; i <='z' ; i++) {       // a,b,c,d----------z
+            System.out.println(i);
+        }
+
+
+        // Enter 5 numbers and have the sum
+        Scanner input = new Scanner(System.in);
+        int sum1 =0;
+
+        /*
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Enter a number please");
+            int number = input.nextInt();
+            sum1+=number;
+        }
+
+        System.out.println("sum1 = " + sum1);
+*/
+
+
+        System.out.println("----------------------");
+
+        //  find the count of "l" in the word (HelloWorld)
+        // output = 3
+
+        String str = "Helloworld";
+        // What is index in the string? What is the letter in index 0 --> index 0 equals -> H
+        // System.out.println(str.charAt(str.length()-1)); This is the method of getting last letter
+
+        int countOfl =0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'l') {
+                countOfl++;
+            }
+        }
+
+        System.out.println("countOfl = " + countOfl);
+
+        System.out.println("----------------------------");
+
+
+        String str1= "john";
+        String str2 = "john";
+
+        // Are these names same, if same print true, if not false
+        // If I compare two strings I am gonna use .equal method.
+
+        if (str1.equals("john")){
+            System.out.println("True");
+        } else  {
+            System.out.println("False");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
