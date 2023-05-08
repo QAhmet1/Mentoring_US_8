5ed4f1ca1ad8db3b00fda4d2189da5d15b32b423
ahmet <ahmetdem6325@gmail.com>
2023-05-08 19:55:23 +0300

    week8/mentoring/Methods overloading

---
+++ src/week8/_1_monday/Example_2.java
@@ -0,0 +1,34 @@
+package week8._1_monday;
+
+import java.util.Scanner;
+
+public class Example_2 {
+    //Get the student name and grades (60 70 90) from the user.
+    // Get 3 students and 3-course grades.
+    // After finding the average of these courses in a method,
+    //the result should be  Ahmet 75.
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 3; i++) {
+
+            System.out.print("Name : ");
+            String name=sc.nextLine();
+            System.out.print("Enter at least 3 grades keep one space between each : ");
+            String strGrades = sc.nextLine();
+            System.out.println("Student Name : "+name+" , Grades : "+findAvg(strGrades));
+        }
+
+    }
+
+    public static int findAvg(String grades){
+        String [] gradesArr=grades.split(" ");
+        int sum=0;
+        int avg;
+        for (int i = 0; i < gradesArr.length; i++) {
+            sum+=Integer.parseInt(gradesArr[i]);
+        }
+        avg=sum/gradesArr.length;
+        return avg;
+    }
+
+}
