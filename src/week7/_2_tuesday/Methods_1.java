b1fb8318a145270155f3bdaecf851f91c06e3749
ahmet <ahmetdem6325@gmail.com>
2023-05-08 19:54:14 +0300

    week8/mentoring/Methods overloading

--- src/week7/_2_tuesday/Methods_1.java
+++ src/week7/_2_tuesday/Methods_1.java
@@ -14,6 +14,7 @@ public class Methods_1 {
          Select an action
         */
     public static void main(String[] args) {
+        
         Scanner sc=new Scanner(System.in);
         int action;
         do {
