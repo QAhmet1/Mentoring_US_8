35f36c32556d39f22e643ce2074b432bb3290eba
emre087 <fee0984@gmail.com>
2023-05-31 18:51:22 +0200

    Mentoring week 8 methods

---
+++ src/wwek11/wednesday/Task1.java
@@ -0,0 +1,71 @@
+package wwek11.wednesday;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        // Array List + Set
+
+          // How can we add element to array list?
+
+        // 1.way-- manually
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+
+        System.out.println(list);
+
+        // 2. way of adding element to array list
+        ArrayList<String> list2 = new ArrayList<>(Arrays.asList("john", "ali"));
+        System.out.println(list2);
+
+
+        ArrayList<Integer> list3 = new ArrayList<>();
+
+        for (int i = 0; i < 100; i++) {
+
+            list3.add(i);
+
+        }
+        System.out.println("list3 = " + list3);
+
+
+
+            // What is the difference between array and array list?
+
+            // 1) Arrays-- size is fixed.. Array list size is dyanmic
+            // 2) Arrays support primitive and non primitive ---> array list supports only non primitive
+            // 3) arrray list is a type Collection  --> Array is a data structure in java
+
+
+        Integer [] arr = new Integer [4];
+        // This is an integer array which has 4 elements
+        // 0 0 0 0
+
+
+        arr[0] = 1;
+        arr[1] = 2;
+
+        System.out.println(Arrays.toString(arr));
+
+        // 1, 2 , 0, 0
+        // 1, 2 .................
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
---
+++ src/wwek11/wednesday/Task2.java
@@ -0,0 +1,82 @@
+package wwek11.wednesday;
+/*
+1.Write a program that can verify if a password is a strong password. Characteristics of strong passwords are:
+        1.1 Password must be at least have 8 characters long, and should not contain space
+        1.2 PassWord should at least contain one upper case letter
+        1.3 PassWord should at least contain one lower case letter
+        1.4 Password should at least contain one special characters
+        1.5 Password should at least contain a digit
+ */
+
+import java.util.ArrayList;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+
+
+        String password = "Kjdsflkjfdsalk29489422";
+
+        // 1.step: I stored string in c a char array list
+        ArrayList<Character> list = new ArrayList<>();
+
+        for (int i = 0; i < password.length(); i++) {
+            list.add(password.charAt(i));
+        }
+
+        System.out.println(list);
+
+        // 2.step
+        boolean r1 = list.size()>=8  && !list.contains(" ");
+        boolean r2 = false;   // has one upper case
+        boolean r3 = false;  // has one lower case
+        boolean r4 = false;  // has at least one special char
+        boolean r5 = false;  // has at least one digit
+
+
+        // 3.Step
+        for (int i = 0; i < list.size(); i++) {
+            // if character is upper case
+            if (Character.isUpperCase(list.get(i))) {
+                    r2 = true;
+            } else if (Character.isLowerCase(list.get(i))) {
+                r3 = true;
+            } else if (Character.isDigit(list.get(i))) {
+                r5 = true;
+            } else {
+                r4 = true;
+            }
+            
+
+        }
+
+        // 4.step
+        boolean isStrongPassword = r1 && r2 && r3 && r4 && r5;   // true true true true true  
+
+
+        System.out.println("isStrongPassword = " + isStrongPassword);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
